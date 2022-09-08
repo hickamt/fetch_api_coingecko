547cf2f2d3a47d16c435394683bc31192c26452b
Todd Hickam <68083052+hickamt@users.noreply.github.com>
2022-09-07 18:38:11 -0700

    Added event listners in main.js and updated the README to reflect the changes.

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # FETCH() API | CoinGecko Data to HTML Table View 
 
-This application serves to fetch() data from CoinGecko's API and output the data to HTML page. It is built upon my onClick() microfrontend example file that illustrates the use of button transitions and onclick event triggers.
+This micro application illustrates the basic method of fetching a data object, JSON in this case, from an API. The API used in this application is: [CoinGecko API Coins by 'id'](https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=binancecoin%2C%20bitcoin%2C%20ethereum%2C%20polkadot%2C%20ripple%2C&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h)
 
-The URL used for the call example is: [CoinGecko API Call](https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=binancecoin%2C%20bitcoin%2C%20ethereum%2C%20polkadot%2C%20ripple%2C&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h)
+Another method used in this application is the "onClick()" event. An older method of handling on click events used a function call within the HTML to the corresponding JavaScript function. Today, events are handled within the JavaScript code using event listeners. When an event occurs, such as clicking on a button, it is JavaScripts job to handle the event for that HTML element.
 
-## To View The Project
+## To View Or Download The Project
 
-In the file folder, find the file named: 'index.html'.
+[fetch(): GitHub Repo](https://github.com/hickamt/fetch_api_coingecko);
+[onClick(): GitHub Repo](https://github.com/hickamt/onClick_cryptoTable_with_transitions)
 
-Double click the index.html file to open it in the browser.
+Download the directory > open the index.html at ./root > serve 'index.html' using the VSCode extension 'Live Serve'.
 
 ## Using Fetch() API
 
@@ -38,4 +39,31 @@ Another method is to build the request and response using standard variable init
 const response = await fetch(coingeckoURL);
 const data = await response.json();
 console.table(data);
+```
+
+## HTML & JS Fetch() Options
+
+[GeeksForGeeks: Overview of Event Listner](https://www.geeksforgeeks.org/difference-between-addeventlistener-and-onclick-in-javascript/)
+
+You can set up an onClick() event in many ways. Two methods that I have used are:
+
+- Traditional in HTML: call your JavaScript method directly from the HTML page. When the event is triggered, the corresponding method is called.
+
+```html
+<button id="show-more" onclick="buildTable()">Show Table</button>
+```
+
+- Modern Event Listner: the JavaScript method is 'actively listening' for the event to occur.
+
+```html
+<button id="show-more" >Show Table</button>
+```
+```js
+// method used in the application
+document.querySelector("#show-more").onclick = buildTable;
+
+// another method of immediately handling the event using Lambda Function
+button.addEventLister("click", () => {
+  //Handle the event, with one or more actions
+})
 ```
--- main.js
+++ main.js
@@ -1,25 +1,44 @@
-
 // Global Scope Variables *******************************
+var tableDataArray = [];
 const coingeckoURL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=binancecoin%2C%20bitcoin%2C%20ethereum%2C%20polkadot%2C%20ripple%2C&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h";
-var cryptoHeaders = ["Name", "Price", "24-hr"];
-var button = document.querySelector("#show-more");
-var cryptoTable = document.querySelector("#crypto-table");
-var tableFlag = true;
+const cryptoTableHeaders = {
+  col_1: "Name",
+  col_2: "Price",
+  col_3: "24-hr",
+};
 // *******************************************************
 
-// Event triggers the table build
-button.onclick = function () {
-  buttonSwitch();
+document.getElementById("show-more").onclick = buildTable;
+document.getElementById("text-search").onkeyup = filterTable;
 
-  if (tableFlag) {
-    buildTable();
-    tableFlag = false;
+// API fetch() -> CoinGecko Cryptcurrency Data
+async function fetchData() {
+  const response = await fetch(coingeckoURL);
+  return await response.json();
+}
+
+// Called from HTML button to fetch() api data
+// and build the data table of cryptocurrency prices
+async function buildTable() {
+  const data = await fetchData(); // Call to CoinGecko API
+  const table = document.getElementById("crypto-table");
+  buttonSwitch(); // change button wording
+  tableHeaders(); // set the table headers
+  tableDataArray = data; // Save CoinGecko data to local array
+
+  clearObjectInnerHTML(table);
+  for (const key in data) {
+    table.innerHTML += tableInnerHTML(key, data);
   }
-};
+}
 
-// Handling Table View Open/Close
+// Opens or Closes the table view based on
+// event 'click' and updates button text
 function buttonSwitch() {
+  const button = document.querySelector("#show-more");
+  const cryptoTable = document.querySelector("#crypto-table-header");
+
   if (cryptoTable.className == "open") {
     //shrink the box
     cryptoTable.className = "";
@@ -31,31 +50,39 @@ function buttonSwitch() {
   }
 }
 
-async function buildTable() {
-  tableHeaders();
-  const data = await fetchData();
-
-  var table = document.getElementById("myTable");
-  for (var i = 0; i < data.length; ++i) {
-    table.innerHTML += `<tr>
-      <td>${data[i].name}</td>
-      <td>${data[i].current_price}</td>
-      <td>${data[i].price_change_24h}</td>
-    </tr>`;
-  }
-}
-
 // Builds the table column headers [ Name, Price, 24-hr ]
 function tableHeaders() {
-  var headers = document.getElementById("table-headers");
+  const tableHeaders = document.getElementById("table-headers");
+  clearObjectInnerHTML(tableHeaders);
 
-  for (var i = 0; i < cryptoHeaders.length; ++i) {
-    headers.innerHTML += `<th>${cryptoHeaders[i]}</th>`;
+  for (const key in cryptoTableHeaders) {
+    tableHeaders.innerHTML += `<th>${cryptoTableHeaders[key]}</th>`;
   }
 }
 
-// Simple fetch() to get CoinGecko Data
-async function fetchData() {
-  const response = await fetch(coingeckoURL);
-  return await response.json();
+// Table is filtered based on user input text.
+function filterTable() {
+  const textInput = document.querySelector("#text-search");
+  const newTable = document.querySelector("#crypto-table");
+  clearObjectInnerHTML(newTable);
+
+  for (const key in tableDataArray) {
+    if (tableDataArray[key].name.includes(textInput.value)) {
+      newTable.innerHTML += tableInnerHTML(key, tableDataArray);
+    }
+  }
+}
+
+// Returns a table row of data as an html string.
+// Arguments: loop index, fetched json data
+function tableInnerHTML(index, data) {
+  return `<tr>
+      <td>${data[index].name}</td>
+      <td>$${parseFloat(data[index].current_price).toFixed(2)}</td>
+      <td>$${parseFloat(data[index].price_change_24h).toFixed(4)}</td>
+    </tr>`;
+}
+
+function clearObjectInnerHTML(htmlObject) {
+  htmlObject.innerHTML = "";
 }
